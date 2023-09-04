a6d4683b50b6614d1c14cb9c2c86c5b17eea9eb5
R Zhong <30917072+Rzz8@users.noreply.github.com>
2023-09-04 10:49:54 +1000

    update local food items

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,13 @@
+import clasess from "./Checkout.module.css";
+
+const Checkout = (props) => {
+  return (
+    <form>
+      <div className={clasess.control}>
+          <label htmlFor=></label>
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
